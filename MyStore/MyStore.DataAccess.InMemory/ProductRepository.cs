75f50f89d060a7d8f0d4619956fadb36200c3d2b
Laser-Sword <38383555+laser-sword@users.noreply.github.com>
2020-08-16 11:19:00 +0100

    Product Manager Pages Built and tested

--- MyStore/MyStore.DataAccess.InMemory/ProductRepository.cs
+++ MyStore/MyStore.DataAccess.InMemory/ProductRepository.cs
@@ -8,7 +8,7 @@ using MyStore.Core.Models;
 
 namespace MyStore.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products;
---
+++ MyStore/MyStore.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyStore.Core.Models;
+using MyStore.DataAccess.InMemory;
+
+namespace MyStore.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        //create an instance of the Product Repository
+        ProductRepository context;
+
+        //crteate an instance of the Productmanagercontroller which isitiates that Product Repository
+        public ProductManagerController() {
+            context = new ProductRepository();
+
+        }
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            //get data from collection and turn it into a list 
+            //and send back into the view
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create() {
+            Product product = new Product();
+            return View(product);
+            
+        }
+        [HttpPost]
+        public ActionResult Create(Product product) {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+
+            }
+            else {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+            [HttpPost]
+            public ActionResult Edit(Product product, string Id) {
+            //find item in database iwth ID
+            Product productToEdit = context.Find(Id);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else {
+                if (!ModelState.IsValid) {
+                    return View(product);
+                }
+                //manually update product
+                productToEdit.Category = product.Category;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+
+                //commit those changes
+                context.Commit();
+
+                return RedirectToAction("Index");
+
+            }
+        }
+        public ActionResult Delete(string Id) {
+            Product productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id) {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+            
+        
+    }
+}
